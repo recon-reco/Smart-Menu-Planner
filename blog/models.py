0f234bcf3dbb1202ffa0f4f5243edbc988089877
recon-reco <psmstat2013@gmail.com>
2024-07-22 20:42:57 +0900

    FBV : Link Post List with Post Detail

--- blog/models.py
+++ blog/models.py
@@ -10,5 +10,8 @@ class Post(models.Model):
     #author : 추후 작성 예정
 
 
-def __str__(self):
-    return f'[{self.pk}]{self.title}'
+    def __str__(self):
+        return f'[{self.pk}]{self.title}'
+
+    def get_absolute_url(self):
+        return f'/blog/{self.pk}'
