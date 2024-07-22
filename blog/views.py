59e43730763805f1c878197ac51600e71d77519e
recon-reco <psmstat2013@gmail.com>
2024-07-23 00:02:08 +0900

    CBV : Post List and Post Detail

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,30 @@
 from django.shortcuts import render
 from .models import Post
-
+from django.views.generic import ListView, DetailView
 # Create your views here.
+
+class PostList(ListView):
+    model = Post
+    ordering='-pk'
+
+class PostDetail(DetailView):
+    model = Post
+
+
+
+"""
 def index(request):
-    posts =  Post.objects.all().order_by("-pk")
+    posts = Post.objects.all().order_by('-pk')
     return render(
         request,
-        'blog/index.html',
-        {
-            'posts':posts
-        },
-
+        'blog/index.html'.
+        {'posts':posts}    
     )
-
 def single_post_page(request,pk):
     post = Post.objects.get(pk=pk)
-
     return render(
         request,
         'blog/single_post_page.html',
-        {
-            'post':post,
-        }
+        {'post':post}
     )
+"""
