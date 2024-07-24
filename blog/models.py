f450bbceb8cf11b62309789c80df9370044fd4e8
recon-reco <psmstat2013@gmail.com>
2024-07-24 13:13:08 +0900

    Creakte Author Field using User Model

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+import os
 
 # Create your models here.
 class Post(models.Model):
@@ -8,10 +10,10 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     #author : 추후 작성 예정
-
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title}::{self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}'
