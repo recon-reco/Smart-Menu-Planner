501808c92fda21ed244d60d8f517a2fb5e9c2432
recon-reco <psmstat2013@gmail.com>
2024-08-16 01:55:43 +0900

    Head Image in Post List page and Post Detail page

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    #head_image
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     #author
     #hook_text
 
