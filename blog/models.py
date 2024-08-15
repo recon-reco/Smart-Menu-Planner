79490449cf9ee0ebd2a659bc39d77071666284dc
recon-reco <psmstat2013@gmail.com>
2024-08-15 17:44:40 +0900

    CBV : Post List and Post Detail html

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    #head_image
+    #author
+    #hook_text
+
+    def __str__(self): #Post Listでpost.title
+        return f'[{self.pk}]{self.title}'
+
+    def get_absolute_url(self, pk):
+        return f'/blog/{self.pk}/'
