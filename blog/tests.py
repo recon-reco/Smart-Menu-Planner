6e6463fe12d6ffcc567fcf321247edcccdffb461
recon-reco <psmstat2013@gmail.com>
2024-08-17 02:23:05 +0900

    error navbar_tes：タイプミス

--- blog/tests.py
+++ blog/tests.py
@@ -7,9 +7,15 @@ class TestView(TestCase):
         self.client=Client()
 
     def navbar_test(self, soup):
-        navbar=soup.nav
+        navbar = soup.nav
         self.assertIn('Blog', navbar.text)
         self.assertIn('About Me', navbar.text)
+
+        blog_btn = navbar.find('a', text='Blog')
+        self.assertEqual(blog_btn.attrs['href'], '/blog/')
+
+        about_me_btn = navbar.find('a', text='About Me')
+        self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
 
     def test_post_list(self):
         ## Post List 仕組み　test
@@ -22,7 +28,7 @@ class TestView(TestCase):
         self.assertEqual(response.status_code,200) 
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(soup.title.text, 'Blog')
-        
+
         self.navbar_test(soup)
 
         ## There is no post
@@ -51,6 +57,7 @@ class TestView(TestCase):
         self.assertEqual(response.status_code, 200)
         print('success :200')
         self.navbar_test(soup)
+
         main_area = soup.find('div', id='main-area')
         print(main_area.text)
         self.assertIn(post_001.title, main_area.text)
