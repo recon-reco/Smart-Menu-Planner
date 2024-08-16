388471d70bd57f71b3d3869e2931c518350d2eb9
recon-reco <psmstat2013@gmail.com>
2024-08-17 02:39:00 +0900

    Model Category 生成

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 import os
 
+class Category(models.Model):
+    name = models.CharField(max_length=10, unique=True)
+    slug = models.SlugField(max_length=100, unique=True, allow_unicode=True)
+    """人が読めるURL生成"""
+    def __str__(self):
+        return self.name
+    
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
@@ -10,6 +17,7 @@ class Post(models.Model):
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d', blank=True)
     author = models.ForeignKey(User, null=True ,on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
     #hook_text = models.CharField(max_length=100, blank=True)
 
     def __str__(self): #Post Listでpost.title
@@ -23,3 +31,4 @@ class Post(models.Model):
     
     def get_file_ext(self):
         return self.get_file_name().split(".")[-1]
+    
